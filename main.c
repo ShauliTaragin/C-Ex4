dad3e601c6a2e34e47ac90a824fd122df39b7da7
Shauli Taragin <shaulitaragin@gmail.com>
2022-01-06 22:06:00 +0200

    fix

--- main.c
+++ main.c
@@ -344,6 +344,7 @@ int main() {
                 getchar();
                 tsp(&head);
                 first=1;
+                getchar();
                 break;
             default:
                 // printGraph(head);
