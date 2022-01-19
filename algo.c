d0d2e66782fbf483b2cf823d4ef493fa95bf91d1
Shauli Taragin <shaulitaragin@gmail.com>
2022-01-19 11:15:30 +0200

    fixed small letter

--- algo.c
+++ algo.c
@@ -2,7 +2,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <limits.h>
-#include "graph.h"
+#include "Graph.h"
 
 //first i wish to create an array to represent our graph so find max node num to create it
 
